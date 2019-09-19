efa502ee3c4f91aa266282dfa72cebc1283d4188
cuber <cuber.carrots@gmail.com>
2019-09-19 20:48:51 +0800

    提交少的可怜的接口

--- ironforge-starter/ironforge-base-stater/src/main/java/org/ironforge/starter/base/model/UserDetails.java
+++ ironforge-starter/ironforge-base-stater/src/main/java/org/ironforge/starter/base/model/UserDetails.java
@@ -1,4 +1,33 @@
 package org.ironforge.starter.base.model;
 
 public class UserDetails {
+
+    /**
+     * 请求ID
+     */
+    private String reqId;
+
+    /**
+     * 用户Id[99999999,99999999]
+     * 后面99999999  代表api访问
+     * 前面代表商户号， 后面代表用户号
+     */
+    private long userId;
+    /**
+     * 商户ID[99999999] 代表系统本身操作
+     */
+    private long merchantId;
+    /**
+     * 用户名称
+     */
+    private String userName;
+    /**
+     * 商户名称
+     */
+    private String merchantName;
+
+    /**
+     * 使用的token
+     */
+    private String token;
 }
